93049f3b5d72cb9f4c6b0cc03b18a6daaad6bc43
major-hub <mrsol1jonovi4@gmail.com>
2022-04-05 16:54:34 +0500

    first installation with Django Suport

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # moscow
+
 Moscow Academy by iFraganus
+
+___
+
+# pip
+
+```
+python3 -m venv venv
+source ./venv/bin/activate
+python -m pip install -U pip
+pip install -r requirements.txt
+```
+
+___
+
+# postgres
+
+```
+CREATE DATABASE moscow WITH OWNER solijonov;
+GRANT ALL ON DATABASE moscow TO solijonov;
+```
+
+___
+
+# Systemd service [moscow.service]
+
+```
+[Unit]
+Description=Systemd service daemon for moscow
+Before=nginx.service
+After=network.target
+[Service]
+User=major
+Group=major
+WorkingDirectory=/home/major/moscow
+# ExecStart=/home/major/moscow/venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:/home/major/moscow/gunicorn.sock project.wsgi:application
+ExecStart=/home/major/moscow/venv/bin/python manage.py runserver 0.0.0.0:8005
+Restart=always
+SyslogIdentifier=gunicorn
+[Install]
+WantedBy=multi-user.target
+```
+
+___
+
+# Nginx [moscow_backend]
+
+```
+server {
+    listen 80;
+    server_name ? www.?;
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static {
+        alias /home/major/moscow/static;
+    }
+    
+    location /media  {
+        alias /home/major/moscow/media;
+    }
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/home/major/moscow/gunicorn.sock;
+    }
+}
+```
+
+___
+
+# Nginx [moscow_frontend]
+
+```
+server {
+    listen 80;
+    server_name ? www.?;
+    root /home/major/frontend/?/build;
+    index index.html index.htm index.nginx-debian.html;
+    location / {
+        try_files $uri $uri/ /index.html;
+        # try_files $uri $uri/ =404;
+    }
+}
+```
---
+++ users/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with 'email' field."""
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email number must be set')
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    # first_name, last_name already have
+    phone_number = models.CharField(max_length=12)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ['-date_joined']
