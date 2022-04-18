d8f3d2a3a00bd60c32755e5c4f1c4ce5e64772f7
major-hub <mrsol1jonovi4@gmail.com>
2022-04-18 10:48:37 +0500

    fixed media document_root

--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('api/users/', include('users.api.urls')),
     path('api/posts/', include('posts.api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
